0dd3c293dabb32fb3fac86846867fd99c15b3dc1
khanhtn <macos@Mac-Pie.local>
2023-04-17 21:00:47 +0700

    add nodemon command

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-const port = 5005;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
