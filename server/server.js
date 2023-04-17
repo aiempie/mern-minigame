e18fde97b207546913de750261b7e62b1845d0eb
khanhtn <macos@Mac-Pie.local>
2023-04-17 20:52:41 +0700

    add npm init & add lib nodemon

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log("demo")
+const express = require("express")
+const app = express()
+
+const port = 5005;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
