1ec1d005b27f3235bcf1ce18f134ab5ba44e50f5
khanhtn <macos@Mac-Pie.local>
2023-04-18 20:11:53 +0700

    add cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server start on port ${port}`);
 });
