717c8b99f65ec8b772c0e5476908d8705e0c69d2
khanhtn <khanhtnfptdn@gmail.com>
2023-05-05 15:03:43 +0700

    create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import AppRouter from "./AppRouter";
+import Navbar from "./pages/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App " data-bs-theme="dark">
-      <header></header>
-      <AppRouter />
+    <div className="App" data-bs-theme="dark">
+      <div className="container">
+        <header>
+          <Navbar />
+        </header>
+        <AppRouter />
+      </div>
     </div>
   );
 }
